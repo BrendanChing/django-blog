10a94ca9369d681637eab2a5b54c0b9cdc890e13
Brendan Ching <brendanching1@gmail.com>
2024-06-13 14:56:25 +0000

    added secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+SECRET_KEY = os.envirn.get("SECRET_key")
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
